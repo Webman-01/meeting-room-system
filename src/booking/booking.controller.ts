3843782c46b3acb30fdc153c18529bdb5a413365
Webman <mamingzhe@MacBook-Pro.local>
2023-12-30 00:16:04 +0800

    D13

---
+++ src/booking/booking.controller.ts
@@ -0,0 +1,93 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  DefaultValuePipe,
+} from '@nestjs/common';
+import { BookingService } from './booking.service';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { UpdateBookingDto } from './dto/update-booking.dto';
+import { generateParseIntPipe } from 'src/utils';
+
+@Controller('booking')
+export class BookingController {
+  constructor(private readonly bookingService: BookingService) {}
+
+  //获取预订列表
+  @Get('list')
+  async list(
+    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
+    pageNo: number,
+    @Query(
+      'pageSize',
+      new DefaultValuePipe(10),
+      generateParseIntPipe('pageSize'),
+    )
+    pageSize: number,
+    @Query('username') username: string,
+    @Query('meetingRoomName') meetingRoomName: string,
+    @Query('meetingRoomPosition') meetingRoomPosition: string,
+    @Query('bookingTimeRangeStart') bookingTimeRangeStart: number,
+    @Query('bookingTimeRangeEnd') bookingTimeRangeEnd: number,
+  ) {
+    return this.bookingService.find(
+      pageNo,
+      pageSize,
+      username,
+      meetingRoomName,
+      meetingRoomPosition,
+      bookingTimeRangeStart,
+      bookingTimeRangeEnd,
+    );
+  }
+
+  //通过申请
+  @Get('apply/:id')
+  async apply(@Param('id') id: number) {
+    return this.bookingService.apply(id);
+  }
+  //拒绝申请
+  @Get('reject/:id')
+  async reject(@Param('id') id: number) {
+    return this.bookingService.reject(id);
+  }
+  //解除
+  @Get('unbind/:id')
+  async unbind(@Param('id') id: number) {
+    return this.bookingService.unbind(id);
+  }
+  //催办
+  @Get('urge/:id')
+  async urge(@Param('id') id: number) {
+    return this.bookingService.urge(id);
+  }
+  @Post()
+  create(@Body() createBookingDto: CreateBookingDto) {
+    return this.bookingService.create(createBookingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.bookingService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bookingService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateBookingDto: UpdateBookingDto) {
+    return this.bookingService.update(+id, updateBookingDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.bookingService.remove(+id);
+  }
+}
